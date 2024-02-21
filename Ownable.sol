75ca4e255ab59663bd1926e1dded990ea04d56ac
Crypto-Crafter <160686348+Crypto-Crafter@users.noreply.github.com>
2024-02-21 10:20:21 +0530

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,15 +1,21 @@
 // SPDX-License-Identifier: MIT
+// Specifies the license under which the code is released (MIT License)
+
 pragma solidity ^0.8.10;
+// Specifies the Solidity compiler version to be used
 
 contract Ownable {
+    // Declares a state variable to store the owner's address
     address public owner;
 
+    // Modifier to restrict access to functions to only the owner
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
+    // Constructor to set the deployer's address as the owner
     constructor() {
         owner = msg.sender;
-    }
+    }
 }
